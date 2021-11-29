5abc7ddeebb6004369c3d486dc82c7788f61b168
Võ Thành Luân <71899498+thanhluan17@users.noreply.github.com>
2021-11-29 18:41:39 +0700

    up b17

---
+++ module4/src/b17_spring_boot/bai_tap/customer_management/src/main/java/com/codegym/cms/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.codegym.cms.service;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.model.Province;
+
+
+public interface CustomerService {
+    Iterable<Customer> findAll();
+
+    Customer findById(Long id);
+
+    void save(Customer customer);
+
+    void remove(Long id);
+
+    Iterable<Customer> findAllByProvince(Province province);
+}
